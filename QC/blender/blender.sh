fd69d2824be9e546744f4eb7c2b2221402e85003
Brian Palmer <bgpalmer14@gmail.com>
2021-10-18 21:36:49 -0700

    The sorts are completing now. Increasing the memory used helped to remove error messages.

--- QC/blender/blender.sh
+++ QC/blender/blender.sh
@@ -5,6 +5,7 @@
 #SBATCH --error="%x_%j.err"
 #SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
+#SBATCH --mem=128G
 source '../../Utilities/Src/bash/logging.sh'
 
 function usage {
@@ -38,43 +39,31 @@ CR2="${INPUT}/105246/merged_nodups.txt.gz"
 LiverMet1="${INPUT}/100887/merged_nodups.txt.gz"
 LiverMet2="${INPUT}/100889/merged_nodups.txt.gz"
 
-echo "SORTING"
+#echo "SORTING"
 
-test -d "${INPUT}/primary_merged" && rm -rf "${INPUT}/primary_merged"
-mkdir "${INPUT}/primary_merged" 
-srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 --pty bash \
-	/usr/bin/time -v sort --parallel=8 -S 20G -m -k1,1d -k5,5d <(gunzip -c $primary1) <(gunzip -c $primary2) <(gunzip -c $primary3) | gzip -c > "${INPUT}/primary_merged/merged_nodups.txt.gz"
+# test -d "${INPUT}/primary_merged" && rm -rf "${INPUT}/primary_merged"
+# mkdir "${INPUT}/primary_merged" 
+# sort --parallel=8 -S 64G -m -k1,1d -k5,5d <(gunzip -c $primary1) <(gunzip -c $primary2) <(gunzip -c $primary3) > "${INPUT}/primary_merged/merged_nodups.txt"
+
+# java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 "${INPUT}/primary_merged/merged_nodups.txt" "${INPUT}/primary_merged/inter.hic" hg38
 
 test -d "${INPUT}/CR_merged" && rm -rf "${INPUT}/CR_merged"
 mkdir "${INPUT}/CR_merged" 
-srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 --pty bash \ 
-	/usr/bin/time -v sort --parallel=8 -S 20G -m -k1,1d -k5,5d <(gunzip -c $CR1) <(gunzip -c $CR2) | gzip -c > "${INPUT}/CR_merged/merged_nodups.txt.gz"
+sort --parallel=8 -S 64G -m -k1,1d -k5,5d <(gunzip -c $CR1) <(gunzip -c $CR2) > "${INPUT}/CR_merged/merged_nodups.txt"
+
+# srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 \ 
+# 	java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 "${INPUT}/CR_merged/merged_nodups.txt.gz" "${INPUT}/CR_merged/inter.hic" hg38
 
 test -d "${INPUT}/livermet_merged" && rm -rf "${INPUT}/livermet_merged"
 mkdir "${INPUT}/livermet_merged" 
-srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=4 --time=4:00:00 --pty bash \ 
-	/usr/bin/time -v sort --parallel=8 -S 20G -m -k1,1d -k5,5d <(gunzip -c $LiverMet1) <(gunzip -c $LiverMet2) | gzip -c > "${INPUT}/livermet_merged/merged_nodups.txt.gz"
+sort --parallel=8 -S 64G -m -k1,1d -k5,5d <(gunzip -c $LiverMet1) <(gunzip -c $LiverMet2) > "${INPUT}/livermet_merged/merged_nodups.txt"
 
-wait
+# srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 \ 
+# 	java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 "${INPUT}/livermet_merged/merged_nodups.txt.gz" "${INPUT}/livermet_merged/inter.hic" hg38
 
-echo "DONE SORTING"
-echo "RUNNING JUICER_TOOLS"
+echo "WAITING FOR SORTS TO FINISH"
 
-srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 --pty bash \ 
-	/usr/bin/time -v java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50" ${INPUT}/primary_merged/merged_nodups.txt.gz" "${INPUT}/primary_merged/inter.hic" hg38
 
-srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 --pty bash \ 
-	/usr/bin/time -v java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 "${INPUT}/CR_merged/merged_nodups.txt.gz" "${INPUT}/CR_merged/inter.hic" hg38
-
-srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 --pty bash \ 
-	/usr/bin/time -v java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 "${INPUT}/livermet_merged/merged_nodups.txt.gz" "${INPUT}/livermet_merged/inter.hic" hg38
-
-wait
-
-echo "JUICER_TOOLS DONE"
 
 exit 0
 
-# sort -m -k1,1d -k5,5d $LM1 $LM2 > LM_merged/merged_nodups.txt.gz
-
-# sort -m -k1,1d -k5,5d ${merged_names} > ${outputdir}/merged_nodups.txt
