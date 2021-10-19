38096a21ba084a1af0566802b5f4300762fdaa61
Brian Palmer <bgpalmer14@gmail.com>
2021-10-19 00:43:00 -0700

    modifying sort to target chromosomes

--- QC/blender/blender.sh
+++ QC/blender/blender.sh
@@ -41,41 +41,29 @@ CR2="${INPUT}/105246/merged_nodups.txt.gz"
 LiverMet1="${INPUT}/100887/merged_nodups.txt.gz"
 LiverMet2="${INPUT}/100889/merged_nodups.txt.gz"
 
-test -d "references" || mkdir "references" 
 
-echo $REFRENCE
-genomeFile=$(basename $REFRENCE)
-test -f "references/$genomeFile" || cp $REFRENCE "references/$genomeFile" 
-
-#echo "SORTING"
-
-# test -d "${INPUT}/primary_merged" && rm -rf "${INPUT}/primary_merged"
-# mkdir "${INPUT}/primary_merged" 
-# sort --parallel=8 -S 64G -m -k1,1d -k5,5d <(gunzip -c $primary1) <(gunzip -c $primary2) <(gunzip -c $primary3) > "${INPUT}/primary_merged/merged_nodups.txt"
+test -d "${INPUT}/primary_merged" && rm -rf "${INPUT}/primary_merged"
+mkdir "${INPUT}/primary_merged" 
+sort --parallel=8 -S 64G -m -k2,2d -k6,6d <(gunzip -c $primary1) <(gunzip -c $primary2) <(gunzip -c $primary3) > "${INPUT}/primary_merged/merged_nodups.txt"
 
 #From the juicer.sh script: In lieu of setting the genome ID, you can instead set the reference sequence and the chrom.sizes file path, 
 # but the directory containing the reference sequence must also contain the BWA index files.
 
 #Usage help: -p chrom.sizes path.
 
-#java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 -p "references/chrom.sizes" "${INPUT}/primary_merged/merged_nodups.txt" "${INPUT}/primary_merged/inter.hic" "references/Homo_sapiens_assembly38.fasta" > /dev/null
+java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 -p "references/chrom.sizes" "${INPUT}/primary_merged/merged_nodups.txt" "${INPUT}/primary_merged/inter.hic" "references/Homo_sapiens_assembly38.fasta" > "primary_skipped.txt"
 
-# test -d "${INPUT}/CR_merged" && rm -rf "${INPUT}/CR_merged"
-# mkdir "${INPUT}/CR_merged" 
-# sort --parallel=8 -S 64G -m -k1,1d -k5,5d <(gunzip -c $CR1) <(gunzip -c $CR2) > "${INPUT}/CR_merged/merged_nodups.txt"
+test -d "${INPUT}/CR_merged" && rm -rf "${INPUT}/CR_merged"
+mkdir "${INPUT}/CR_merged" 
+sort --parallel=8 -S 64G -m -k2,2d -k6,6d <(gunzip -c $CR1) <(gunzip -c $CR2) > "${INPUT}/CR_merged/merged_nodups.txt"
 
-# srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 \ 
-java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 -p "references/chrom.sizes" "${INPUT}/CR_merged/merged_nodups.txt" "${INPUT}/CR_merged/inter.hic" "references/Homo_sapiens_assembly38.fasta" > /dev/null
+java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 -p "references/chrom.sizes" "${INPUT}/CR_merged/merged_nodups.txt" "${INPUT}/CR_merged/inter.hic" "references/Homo_sapiens_assembly38.fasta" > "cr_skipped.txt"
 
-# test -d "${INPUT}/livermet_merged" && rm -rf "${INPUT}/livermet_merged"
-# mkdir "${INPUT}/livermet_merged" 
-# sort --parallel=8 -S 64G -m -k1,1d -k5,5d <(gunzip -c $LiverMet1) <(gunzip -c $LiverMet2) > "${INPUT}/livermet_merged/merged_nodups.txt"
+test -d "${INPUT}/livermet_merged" && rm -rf "${INPUT}/livermet_merged"
+mkdir "${INPUT}/livermet_merged" 
+sort --parallel=8 -S 64G -m -k2,2d -k6,6d <(gunzip -c $LiverMet1) <(gunzip -c $LiverMet2) > "${INPUT}/livermet_merged/merged_nodups.txt"
 
-# srun -A bgmp -p bgmp --nodes=1 --ntasks-per-node=1 --cpus-per-task=8 --time=4:00:00 \ 
-#java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 -p "references/chrom.sizes" "${INPUT}/livermet_merged/merged_nodups.txt" "${INPUT}/livermet_merged/inter.hic" "references/Homo_sapiens_assembly38.fasta"
-
-echo "WAITING FOR SORTS TO FINISH"
-
+java -Xmx2g -jar juicer_tools.jar pre -q 1 -r 25 50 -p "references/chrom.sizes" "${INPUT}/livermet_merged/merged_nodups.txt" "${INPUT}/livermet_merged/inter.hic" "references/Homo_sapiens_assembly38.fasta" > "met_skipped.txt"
 
 
 exit 0
