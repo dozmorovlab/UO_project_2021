8bb964da4bbcb42eacc6a3a55017c1078ef8f5c9
Jonathan Ogata <85324013+The-Hungry-Caterpillar@users.noreply.github.com>
2021-12-09 12:03:34 -0800

    Update tad_stats.py

--- Differential_TADs/tad_stats.py
+++ Differential_TADs/tad_stats.py
@@ -7,13 +7,6 @@ def get_args(): #defines all the independent variables
 	return parser.parse_args()
 args=get_args()
 
-control_dict={
-	#chromosome:(average_tad_size, number of tads)
-}
-treatment_dict={
-	#chromosome:(average_tad_size, number of tads)
-}
-
 chromosomes=[]
 
 
@@ -56,7 +49,6 @@ with open(args.outdir+'/control_domains.bed') as control:
 			control_sizes.append(avg_tad_size)
 			control_numbers.append(number_tads)
 			chromosomes.append(prev_chrom)
-			# control_dict[prev_chrom]=(avg_tad_size,number_tads)
 			tad_sizes.clear()
 
 		prev_chrom=chromosome
@@ -98,7 +90,6 @@ with open(args.outdir+'/treatment_domains.bed') as treatment:
 			number_tads=len(tad_sizes)
 			treatment_sizes.append(avg_tad_size)
 			treatment_numbers.append(number_tads)
-			# control_dict[prev_chrom]=(avg_tad_size,number_tads)
 			tad_sizes.clear()
 
 		prev_chrom=chromosome
@@ -142,7 +133,6 @@ for i in range(0,len(chromosomes)):
 
 
 ################# Getting stuff from accepted TADs ##############
-# accepted_sizes=[]
 accepted_numbers=[]
 tad_sizes=[]
 prev_chrom='1'
@@ -155,9 +145,7 @@ with open(args.outdir+'/differential_TAD_accepted.bed') as accepted:
 		line=accepted.readline().strip().split('\t')
 		
 		if line ==['']:
-			# avg_tad_size=round(st.mean(tad_sizes),0)
 			number_tads=len(tad_sizes)
-			# accepted_sizes.append(avg_tad_size)
 			accepted_numbers.append(number_tads)
 			break
 		
@@ -168,17 +156,13 @@ with open(args.outdir+'/differential_TAD_accepted.bed') as accepted:
 		tad_sizes.append(size_of_tad)
 
 		if chromosome != prev_chrom:
-			# avg_tad_size=round(st.mean(tad_sizes),0)
 			number_tads=len(tad_sizes)
-			# accepted_sizes.append(avg_tad_size)
 			accepted_numbers.append(number_tads)
-			# control_dict[prev_chrom]=(avg_tad_size,number_tads)
 			tad_sizes.clear()
 
 		prev_chrom=chromosome
 
 ################# Getting stuff from rejected TADs ##############
-# rejected_sizes=[]
 rejected_numbers=[]
 tad_sizes=[]
 prev_chrom='1'
@@ -191,9 +175,7 @@ with open(args.outdir+'/differential_TAD_rejected.bed') as rejected:
 		line=rejected.readline().strip().split('\t')
 		
 		if line ==['']:
-			# avg_tad_size=round(st.mean(tad_sizes),0)
 			number_tads=len(tad_sizes)
-			# rejected_sizes.append(avg_tad_size)
 			rejected_numbers.append(number_tads)
 			break
 		
@@ -204,11 +186,8 @@ with open(args.outdir+'/differential_TAD_rejected.bed') as rejected:
 		tad_sizes.append(size_of_tad)
 
 		if chromosome != prev_chrom:
-			# avg_tad_size=round(st.mean(tad_sizes),0)
 			number_tads=len(tad_sizes)
-			# rejected_sizes.append(avg_tad_size)
 			rejected_numbers.append(number_tads)
-			# control_dict[prev_chrom]=(avg_tad_size,number_tads)
 			tad_sizes.clear()
 
 		prev_chrom=chromosome
@@ -275,33 +254,6 @@ plt.xticks([r + barWidth for r in range(len(chromosomes))],
  
 plt.legend()
 plt.savefig('tad_numbers.png')
-
-
-
-# #################### plotting accepted/rejected numbers #####################
-
-# # set width of bar
-# barWidth = 0.35
-# fig = plt.subplots(figsize =(12, 8))
- 
-# # Set position of bar on X axis
-# br1 = np.arange(len(chromosomes))
-# br2 = [x + barWidth for x in br1]
- 
-# # Make the plot
-# plt.bar(br1, accepted_numbers, color ='gold', width = barWidth,
-#         edgecolor ='black', label ='Accepted')
-# plt.bar(br2, rejected_numbers, color ='darkorchid', width = barWidth,
-#         edgecolor ='black', label ='Rejected')
- 
-# # Adding Xticks
-# plt.xlabel('Chromosome', fontweight ='bold', fontsize = 15)
-# plt.ylabel('Number of TADs', fontweight ='bold', fontsize = 15)
-# plt.xticks([r + barWidth for r in range(len(chromosomes))],
-#         chromosomes)
- 
-# plt.legend()
-# plt.savefig('differential_numbers.png')
 
 
 #################### plotting ratio rejected/accepted numbers #####################
