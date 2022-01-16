43ef0268d02be8352e98bc6fffa26db3ca657715
Jonathan Ogata <85324013+The-Hungry-Caterpillar@users.noreply.github.com>
2022-01-16 10:33:57 -0800

    Update diff_TADs.sh

--- Differential_TADs/diff_TADs.sh
+++ Differential_TADs/diff_TADs.sh
@@ -19,38 +19,47 @@
 
 conda activate hicexplorer
 
+#####################################################################
 ##### Options #####
-# treatment sample name
-treatment_name=livermet 
+#####################################################################
+# resolution
+res=50000
+
+# min depth: Minimum window length (in bp) to be considered to the left and to the right of each Hi-C bin. This number should be at least 3 times as large as the bin size of the Hi-C matrix
+# max depth: Maximum window length to be considered to the left and to the right of the cut point in bp. This number should around 6-10 times as large as the bin size of the Hi-C matrix.
+let min_depth=6*$res
+let max_depth=12*$res
+let min_bound=$res
+threshold=0.007
+delta=0.1
+
+# out directory
+out_dir=./diff_TADs_$res
+mkdir -p $out_dir
 
 # control sample name
 control_name=primary 
 
-# resolution
-res=25000
-
-# out directory
-out_dir=./diff_TADs_$res
+# treatment sample name
+treatment_name=livermet 
 
 # path to treatment hic matrix
-treatment_hic_matrix=/projects/bgmp/shared/2021_projects/VCU/week2/livermet_merged/inter_25_50.hic
+treatment_hic_matrix=/projects/bgmp/shared/2021_projects/VCU/data/merged_files/livermet_merged/inter_25_50.hic
 
 # path to control hic matrix
-control_hic_matrix=/projects/bgmp/shared/2021_projects/VCU/week2/primary_merged/sorted/primary_sorted_half1.hic
+control_hic_matrix=/projects/bgmp/shared/2021_projects/VCU/data/merged_files/primary_merged/text_files/down_sampled/splitted_shuffled_primary_downsample_1.hic
 
-
-# the following lines do not need changing
 a=_$res
-treatment_cool_matrix=$out_dir/$treatment_name$a.cool
-control_cool_matrix=$out_dir/$control_name$a.cool
-treatment_corrected_matrix=$out_dir/ICE_corrected_$treatment_name.cool
-control_corrected_matrix=$out_dir/ICE_corrected_$control_name.cool
+#####################################################################
+#####################################################################
 
 
-mkdir -p $out_dir
 
 
-# converts treatment hic to treatment cool
+
+#####################################################################
+# converts hic matrices to cool matrices
+#####################################################################
 hicConvertFormat \
 --matrices $treatment_hic_matrix \
 --inputFormat hic \
@@ -58,8 +67,6 @@ hicConvertFormat \
 --outputFormat cool \
 --resolutions $res
 
-
-# converts control hic to control cool
 hicConvertFormat \
 --matrices $control_hic_matrix \
 --inputFormat hic \
@@ -68,79 +75,126 @@ hicConvertFormat \
 --resolutions $res
 
 
+echo 'done with converting'
+#####################################################################
+#####################################################################
+
+treatment_cool_matrix=$out_dir/$treatment_name$a.cool
+control_cool_matrix=$out_dir/$control_name$a.cool
+
+
+
+
+#####################################################################
 # Normalize matrices to lowest read count of the given matrices
+#####################################################################
 hicNormalize \
 --matrices $treatment_cool_matrix $control_cool_matrix \
 --normalize smallest \
--o $treatment_cool_matrix $control_cool_matrix
+-o $out_dir/norm_$treatment_name.cool $out_dir/norm_$control_name.cool
 
 
-# ICE correction and review of count frequencies for treatment matrix
+echo 'dont with normalize'
+#####################################################################
+#####################################################################
+treatment_cool_matrix=$out_dir/norm_$treatment_name.cool
+control_cool_matrix=$out_dir/norm_$control_name.cool
+
+
+
+
+
+#####################################################################
+# ICE correction and review of count frequencies
+#####################################################################
+hicCorrectMatrix diagnostic_plot \
+--matrix $treatment_cool_matrix \
+-o $out_dir/treatment_diagnostic_before
+
+hicCorrectMatrix diagnostic_plot \
+--matrix $control_cool_matrix \
+-o $out_dir/control_diagnostic_before
+
 hicCorrectMatrix correct \
 -m $treatment_cool_matrix \
--o $out_dir/ICE_corrected_$treatment_name.cool \
---filterThreshold -1 5 \
+-o $out_dir/corrected_$treatment_name.cool \
+--filterThreshold -0.5 5 \
 --correctionMethod ICE 
 
-
-# ICE correction and review of count frequencies for control matrix
 hicCorrectMatrix correct \
 -m $control_cool_matrix \
--o $out_dir/ICE_corrected_$control_name.cool \
---filterThreshold -1 5 \
+-o $out_dir/corrected_$control_name.cool \
+--filterThreshold -0.5 5 \
 --correctionMethod ICE
 
+hicCorrectMatrix diagnostic_plot \
+--matrix $out_dir/ICE_corrected_$treatment_name.cool \
+-o $out_dir/treatment_diagnostic_after
 
-# # finds PCA 1 and 2 for treatment matrix
-# hicPCA \
-# -m $treatment_corrected_matrix \
-# --format bigwig \
-# -o $out_dir/treatment_pca1.bw $out_dir/treatment_pca2.bw
-
-
-# # finds PCA 1 and 2 for control matrix
-# hicPCA \
-# -m $control_corrected_matrix \
-# --format bigwig \
-# -o $out_dir/control_pca1.bw $out_dir/control_pca2.bw
+hicCorrectMatrix diagnostic_plot \
+--matrix $out_dir/ICE_corrected_$control_name.cool \
+-o $out_dir/control_diagnostic_after
 
 
-# min depth: Minimum window length (in bp) to be considered to the left and to the right of each Hi-C bin. This number should be at least 3 times as large as the bin size of the Hi-C matrix
-# max depth: Maximum window length to be considered to the left and to the right of the cut point in bp. This number should around 6-10 times as large as the bin size of the Hi-C matrix.
-let min_depth=4*$res
-let max_depth=9*$res
+echo 'done with ICE correcting'
+#####################################################################
+#####################################################################
 
 
-# removes previous tad scores
+
+
+
+# #####################################################################
+# # KR correction for matrices
+# #####################################################################
+# hicCorrectMatrix correct \
+# -m $treatment_cool_matrix \
+# -o $out_dir/corrected_$treatment_name.cool \
+# --correctionMethod KR
+
+# hicCorrectMatrix correct \
+# -m $control_cool_matrix \
+# -o $out_dir/corrected_$control_name.cool \
+# --correctionMethod KR
+
+
+# echo 'done with KR correction'
+# #####################################################################
+# #####################################################################
+
+
+
+treatment_corrected_matrix=$out_dir/corrected_$treatment_name.cool
+control_corrected_matrix=$out_dir/corrected_$control_name.cool
+
+
+
+#####################################################################
+# find TADs on each matrix and compute differential TADs
+#####################################################################
+removes previous tad scores
 rm $out_dir/treatment_tad_score.bm
 rm $out_dir/control_tad_score.bm
 
-
-# finds TAD domains on treatment file, only needed for treatment matrix according to:
-# https://hicexplorer.readthedocs.io/en/latest/content/tools/hicDifferentialTAD.html
 hicFindTADs \
 -m $treatment_corrected_matrix \
 --outPrefix $out_dir/treatment \
 --correctForMultipleTesting fdr \
---thresholdComparisons 0.05 \
---delta 0.05 \
+--thresholdComparisons $threshold \
+--delta $delta \
 --step $res \
 --minDepth $min_depth \
 --maxDepth $max_depth
 
-
-# finds TAD domains on control file
 hicFindTADs \
 -m $control_corrected_matrix \
 --outPrefix $out_dir/control \
 --correctForMultipleTesting fdr \
---thresholdComparisons 0.05 \
---delta 0.05 \
+--thresholdComparisons $threshold \
+--delta $delta \
 --step $res \
 --minDepth $min_depth \
 --maxDepth $max_depth
-
-
 
 # Computes differential TADs by comparing the precomputed TAD regions of the target matrix with the same regions of the control matrix.
 hicDifferentialTAD \
@@ -152,35 +206,65 @@ hicDifferentialTAD \
 --pValue 0.02 \
 -o $out_dir/differential_TAD
 
-
-# rename to .bed
+# rename diff tad files to .bed
 mv $out_dir/differential_TAD_accepted.diff_tad $out_dir/differential_TAD_accepted.bed
 mv $out_dir/differential_TAD_rejected.diff_tad $out_dir/differential_TAD_rejected.bed
 
+echo 'done with finding TADs and differential TADs'
+#####################################################################
+#####################################################################
 
+
+
+
+
+#####################################################################
+# plot TAD results
+#####################################################################
 cp tracks.ini ./$out_dir
-
 # uses tracks.ini file to plot hic matrices and TADs (tracks.ini file needs to be manually created)
 pyGenomeTracks \
 --tracks $out_dir/tracks.ini \
---out $out_dir/diff_tads.png \
---region chr20:35000000-45000000
+--out $out_dir/diff_tads_mindepth$min_depth.maxdepth$max_depth.minbound$min_distance.delta$delta.threshold$threshold.png \
+--region chr20:40000000-45000000
 
-# # this plots the interaction matrix along with 1st and 2nd eigenvectors
-# hicPlotMatrix \
-# -m $treatment_corrected_matrix \
-# --bigwig $out_dir/treatment_pca2.bw \
-# -o $out_dir/$treatment_name.png \
-# --log1p \
-# --chromosomeOrder 20
+echo 'done with plotting'
+#####################################################################
+#####################################################################
 
-# # this plots the interaction matrix along with 1st and 2nd eigenvectors
-# hicPlotMatrix \
-# -m $control_corrected_matrix \
-# --bigwig $out_dir/control_pca2.bw \
-# -o $out_dir/$control_name.png \
-# --log1p \
-# --chromosomeOrder 20
 
-# custom python script that finds average size TAD and number TADs per chromosome
+
+
+
+#####################################################################
+# plot all cool matrices
+#####################################################################
+
+hicPlotMatrix \
+-m $treatment_corrected_matrix \
+-o $out_dir/chr20_$treatment_name.png \
+--log1p \
+--chromosomeOrder 20
+
+hicPlotMatrix \
+-m $control_corrected_matrix \
+-o $out_dir/chr20_$control_name.png \
+--log1p \
+--chromosomeOrder 20
+
+echo 'done with heatmaps'
+#####################################################################
+#####################################################################
+
+
+
+
+
+#####################################################################
+# custom python script to find avg TAD size, differential TAD sizes, and number of TADs
+#####################################################################
+
 python tad_stats.py -o $out_dir
+
+#####################################################################
+#####################################################################
